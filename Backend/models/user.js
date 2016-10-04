04ecbea345871fbf852fc548afa42d4fd2327cba
bearyshan <bearyshan@ecs.vuw.ac.nz>
2016-10-05 10:36:40 +1300

    Encrypting password sucessfully

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
-
 var mongoSchema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 
 var userSchema = new mongoSchema({
   username : { type:String, required: true, unique: true },
@@ -11,5 +12,40 @@ var userSchema = new mongoSchema({
   admin : Boolean,
   created_at : { type: Date, default: Date.now }
 });
+
+userSchema.pre("save", function(next) {
+  var user = this;
+
+  //only hash the password if it has been modified or new
+  if(!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    //Hash the password using new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      //override cleartext password with hash one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password , function(err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('User', userSchema);
